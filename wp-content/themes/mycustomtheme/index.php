48ace6290e4217cd7bea1561f0dc52cc5e1f8872
MarkoSimka <markosimonoski8173@gmail.com>
2024-08-06 22:48:33 +0200

    [MS] Upgraded and fixed form fields

--- wp-content/themes/mycustomtheme/index.php
+++ wp-content/themes/mycustomtheme/index.php
@@ -18,35 +18,50 @@
             <label for="firstName">First Name:</label>
             <input type="text" id="firstName" name="firstName" required>
             <label for="lastName">Last Name:</label>
+            <label for="firstName">First Name:</label>
+            <input type="text" id="firstName" name="firstName" required>
+
+            <label for="lastName">Last Name:</label>
             <input type="text" id="lastName" name="lastName" required>
-            <label for="mobile">Email:</label>
-            <input type="text" id="mobile" name="mobile">
+
             <label for="email">Email:</label>
             <input type="email" id="email" name="email">
-            <label for="lead_source">First Name:</label>
+
+            <label for="lead_source">Lead Source:</label>
             <input type="text" id="lead_source" name="lead_source">
-            <label for="salutation">Last Name:</label>
+
+            <label for="salutation">Salutation:</label>
             <input type="text" id="salutation" name="salutation">
-            <label for="dob">dob:</label>
-            <input type="text" id="dob" name="dob">
-            <label for="county">First Name:</label>
+
+            <label for="dob">Date of Birth:</label>
+            <input type="date" id="dob" name="dob">
+
+            <label for="county">County:</label>
             <input type="text" id="county" name="county">
-            <label for="country">Last Name:</label>
+
+            <label for="country">Country:</label>
             <input type="text" id="country" name="country">
-            <label for="city">city:</label>
+
+            <label for="city">City:</label>
             <input type="text" id="city" name="city">
-            <label for="telephone">telephone:</label>
+
+            <label for="telephone">Telephone:</label>
             <input type="tel" id="telephone" name="telephone">
-            <label for="mobile">mobile:</label>
-            <input type="text" id="mobile" name="mobile">
-            <label for="address">address:</label>
+
+            <label for="mobile">Mobile:</label>
+            <input type="tel" id="mobile" name="mobile">
+
+            <label for="address">Address:</label>
             <input type="text" id="address" name="address">
-            <label for="post_code">post_code:</label>
+
+            <label for="post_code">Post Code:</label>
             <input type="text" id="post_code" name="post_code">
-            <label for="treatment_interest">treatment_interest:</label>
+
+            <label for="treatment_interest">Treatment Interest:</label>
             <input type="text" id="treatment_interest" name="treatment_interest">
-            <label for="opt_email">opt_email:</label>
-            <input type="text" id="opt_email" name="opt_email">
+
+            <label for="opt_email">Opt-in Email:</label>
+            <input type="checkbox" id="opt_email" name="opt_email">
             <button type="submit">Submit</button>
         </form>
         <div id="formMessage"></div>
