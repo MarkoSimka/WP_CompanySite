248edb59b7afc96871ee2e7df2f0e471e8553440
MarkoSimka <markosimonoski8173@gmail.com>
2024-08-07 16:17:21 +0200

    [MS] Resolving CORS and internal erros

---
+++ wp-content/themes/mycustomtheme/proxy.php
@@ -0,0 +1,29 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+$apiUrl = 'https://regression.pabau.me/OAuth2/leads/lead-curl.php';
+$apiKey = 'REDACTED_API_KEY';
+
+// Read the incoming POST data
+$postData = file_get_contents("php://input");
+
+// Initialize cURL
+$ch = curl_init($apiUrl);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'apiKey: ' . $apiKey
+));
+
+// Execute the request
+$response = curl_exec($ch);
+
+// Close cURL session
+curl_close($ch);
+
+// Output the response
+echo $response;
