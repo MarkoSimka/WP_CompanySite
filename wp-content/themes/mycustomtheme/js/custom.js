dc707b4bce7a47dae1fac67ebe724b72277e237f
MarkoSimka <markosimonoski8173@gmail.com>
2024-08-07 21:41:39 +0200

    [MS] Upgraded the form and added proxy to communicate with the api

--- wp-content/themes/mycustomtheme/js/custom.js
+++ wp-content/themes/mycustomtheme/js/custom.js
@@ -2,40 +2,38 @@ jQuery(document).ready(function ($) {
   $("#leadForm").on("submit", function (e) {
     e.preventDefault();
 
-    var formData = {
-      firstName: $("#firstName").val(),
-      lastName: $("#lastName").val(),
-      mobile: $("#mobile").val(),
-      email: $("#email").val(),
-      lead_source: $("#lead_source").val(),
-      salutation: $("#salutation").val(),
-      dob: $("#dob").val(),
-      county: $("#county").val(),
-      country: $("#country").val(),
-      city: $("#city").val(),
-      telephone: $("#telephone").val(),
-      address: $("#address").val(),
-      post_code: $("#post_code").val(),
-      treatment_interest: $("#treatment_interest").val(),
-      opt_email: $("#opt_email").is(":checked") ? 1 : 0,
-      opt_letter: $("#opt_letter").is(":checked") ? 1 : 0,
-      opt_sms: $("#opt_sms").is(":checked") ? 1 : 0,
-      opt_newsletter: $("#opt_newsletter").is(":checked") ? 1 : 0,
-      opt_phone: $("#opt_phone").is(":checked") ? 1 : 0,
-      redirect_link: $("#redirect_link").val(),
-    };
+    var fd = new FormData();
+    fd.append("api_key", "MTIzMDgbd283795c5e51d3b7efd4aefbc404fb8");
+    fd.append("firstName", $("#firstName").val());
+    fd.append("lastName", $("#lastName").val());
+    fd.append("email", $("#email").val());
+    fd.append("lead_source", $("#lead_source").val());
+    fd.append("salutation", $("#salutation").val());
+    fd.append("dob", $("#dob").val());
+    fd.append("county", $("#county").val());
+    fd.append("country", $("#country").val());
+    fd.append("city", $("#city").val());
+    fd.append("telephone", $("#telephone").val());
+    fd.append("mobile", $("#mobile").val());
+    fd.append("address", $("#address").val());
+    fd.append("post_code", $("#post_code").val());
+    fd.append("treatment_interest", $("#treatment_interest").val());
+    fd.append("opt_email", $("#opt_email").is(":checked") ? 1 : 0);
+    fd.append("redirect_link", $("#redirect_link"));
 
     $.ajax({
-      url: "https://regression.pabau.me/OAuth2/leads/lead-curl.php",
+      url: "/wordpress/wp-content/themes/mycustomtheme/proxy.php",
       method: "POST",
-      crossDomain: true,
-      contentType: "application/json",
-      data: JSON.stringify(formData),
+      data: fd,
+      processData: false,
+      contentType: false,
       success: function (response) {
-        $("#formMessage").html("<p>Lead created successfully!</p>");  
-        window.location.href = $("#redirect_link").val();
+        console.log(response);
+        $("#formMessage").html("<p>Lead created successfully!</p>");
+        // window.location.href = $("#redirect_link").val();
       },
       error: function (xhr, status, error) {
+        console.log(xhr.responseText, status, error);
         $("#formMessage").html("<p>Error: " + xhr.responseText + "</p>");
       },
     });
--- wp-content/themes/mycustomtheme/proxy.php
+++ wp-content/themes/mycustomtheme/proxy.php
@@ -4,23 +4,63 @@ header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
 $apiUrl = 'https://regression.pabau.me/OAuth2/leads/lead-curl.php';
-$apiKey = 'MTIzMDgbd283795c5e51d3b7efd4aefbc404fb8';
 
-// Read the incoming POST data
-$postData = file_get_contents("php://input");
+// Collect the POST data
+$apiKey = $_POST['api_key'] ?? '';
+$firstName = $_POST['firstName'] ?? '';
+$lastName = $_POST['lastName'] ?? '';
+$email = $_POST['email'] ?? '';
+$lead_source = $_POST['lead_source'] ?? '';
+$salutation = $_POST['salutation'] ?? '';
+$dob = $_POST['dob'] ?? '';
+$county = $_POST['county'] ?? '';
+$country = $_POST['country'] ?? '';
+$city = $_POST['city'] ?? '';
+$telephone = $_POST['telephone'] ?? '';
+$mobile = $_POST['mobile'] ?? '';
+$address = $_POST['address'] ?? '';
+$post_code = $_POST['post_code'] ?? '';
+$treatment_interest = $_POST['treatment_interest'] ?? '';
+$opt_email = $_POST['opt_email'] ?? '';
+
+// Prepare the data for cURL
+$data = [
+    'api_key' => $apiKey,
+    'Fname' => $firstName,
+    'Lname' => $lastName,
+    'email' => $email,
+    'lead_source' => $lead_source,
+    'salutation' => $salutation,
+    'dob' => $dob,
+    'county' => $county,
+    'country' => $country,
+    'city' => $city,
+    'telephone' => $telephone,
+    'mobile' => $mobile,
+    'address' => $address,
+    'post_code' => $post_code,
+    'treatment_interest' => $treatment_interest,
+    'opt_email' => $opt_email
+];
+
+print_r($data);
 
 // Initialize cURL
 $ch = curl_init($apiUrl);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Content-Type: application/json',
-    'apiKey: ' . $apiKey
+    'Content-Type: application/x-www-form-urlencoded'
 ));
 
 // Execute the request
 $response = curl_exec($ch);
+
+// Check for errors
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
 
 // Close cURL session
 curl_close($ch);
